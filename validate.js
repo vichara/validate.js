6443df363940b1d4f65f89633a952cad41b08b2d
vichara <vicharas@gmail.com>
2015-09-02 17:03:14 +0900

    Update validate.js

--- validate.js
+++ validate.js
@@ -2,6 +2,8 @@
     var 
         i = 0
         ,len
+        ,fn = 'validate'
+        ,resultArr
         ,validate = {
             
               config : {
@@ -24,17 +26,17 @@
             }
             
             , isEmpty : function(val) {
-                if (val === undefined || val === null) {
+                if (!val) {
                 	return true;
                 }
                 
-                if (typeof (val) == 'string' && this.trim(val) === '') {
+                if (typeof (val) === 'string' && this.trim(val) === '') {
                 	return true;
                 }
                 
-                if (typeof (val) == 'function' 
-                    || typeof (val) == 'number' 
-                        || typeof (val) == 'boolean' 
+                if (typeof (val) === 'function' 
+                    || typeof (val) === 'number' 
+                        || typeof (val) === 'boolean' 
                             || Object.prototype.toString.call(val) === '[object Date]') {
                 	return false;
                 }
@@ -73,7 +75,43 @@
                 }
             }
             
-            , fieldValidation : function (objs, attr) {
+            , fieldValidation : function (_objs, _callBack) {
+            	var callBack = _callBack || function (inp, message){
+            		alert(message);
+            		inp.focus();
+            		return false;
+            	};
+            	
+            	if (!_objs){
+            		return false;
+            	}
+            	
+            	if (Object.prototype.toString.call( _objs ) !== '[object Array]') {
+            		return false;
+            	}
+            	
+            	resultArr = [];
+            	
+            	var objs,attr,result_inp,subBoo = false;
+            	for (i = 0, len = _objs.length; i < len; i++) {
+            		objs = _objs[i]['inp']
+            		_objs[i]['inp'] = undefined;
+            		attr = _objs[i];
+            		subBoo = this.fieldValidationSub(objs, attr);
+            		if (!subBoo) {
+            			break;
+            		}
+            	}
+            	
+            	if ( typeof callBack  === 'function' && !resultArr) {
+            		callBack(resultArr[0], resultArr[1]);
+            	}
+            	
+            	return subBoo;
+            }
+            
+            
+            , fieldValidationSub : function (objs, attr) {
                 this.console('start');
                 
                 if (!objs || !attr) {
@@ -87,38 +125,72 @@
                         obj = objs[i];
                         
                         //노드 체크
-                        if (!this.checkNode(obj)) return false;
+                        if (!this.checkNode(obj)) {
+                        	return false;
+                        }
                         
                         //타이틀 유무 설정
-                        if (!this.checkTitle(obj, attr)) return false;
+                        if (!this.checkTitle(obj, attr)) {
+                        	return false;
+                        }
                         
                         //필수 입력값 체크
-                        if (!this.validationRequired(obj, attr)) return false;
+                        if (!this.validationRequired(obj, attr)) {
+                        	return false;
+                        }
                         
                         //길이 체크
-                        if (!this.validationLength(obj, attr)) return false;
+                        if (!this.validationLength(obj, attr)) {
+                        	return false;
+                        }
                     }
                 }
                 else {
                     obj = objs;
                     
                     //노드 체크
-                    if (!this.checkNode(obj)) return false;
+                    if (!this.checkNode(obj)) {
+                    	return false;
+                    }
                     
                     //타이틀 유무 설정
-                    if (!this.checkTitle(obj, attr)) return false;
+                    if (!this.checkTitle(obj, attr)) {
+                    	return false;
+                    }
                     
                     //필수 입력값 체크
-                    if (!this.validationRequired(obj, attr)) return false;
+                    if (!this.validationRequired(obj, attr)) {
+                    	return false;
+                    }
                     
                     //길이 체크
-                    if (!this.validationLength(obj, attr)) return false;
+                    if (!this.validationLength(obj, attr)) {
+                    	return false;
+                    }
                 }
                 
                 return true;
             }
             
-            , eventHandler : function (objs, attr) {
+            , eventHandler : function (arr) {
+            	if (!arr){
+            		return;
+            	}
+            	
+            	if (Object.prototype.toString.call( arr ) !== '[object Array]') {
+            		return;
+            	}
+            	
+            	var objs,attr;
+            	for (i = 0, len = arr.length; i < len; i++) {
+            		objs = arr[i]['inp']
+            		arr[i]['inp'] = undefined;
+            		attr = arr[i];
+            		this.eventHandlerSub(objs, attr);
+            	}
+            }
+            	
+            , eventHandlerSub : function (objs, attr) {
                 this.console('eventHandler start');
                 
                 if (!objs || !attr) {
@@ -132,30 +204,35 @@
                     for (i=0, len = objs.length; i<len; i++) {
                         obj = objs[i];
                         //노드 체크
-                        if (!this.checkNode(obj)) return;
+                        if (!this.checkNode(obj)) {
+                        	return;
+                        }
                         
                         this.addEventListener(obj, function (){
                             //길이 체크
-                            validate.validationLength(obj, attr, true);
+                        	thisObj.validationLength(obj, attr, true);
                             
                             //대소문자 변경
-                            validate.toCase(obj, attr);
+                        	thisObj.toCase(obj, attr);
                             
                         });
                     }
                 }
                 else {
-                    obj = objs;
+                    obj = objs
+                    ,thisObj = this;
                     
                     //노드 체크
-                    if (!this.checkNode(obj)) return;
+                    if (!this.checkNode(obj)) {
+                    	return;
+                    }
                     
                     this.addEventListener(obj, function (){
                         //길이 체크
-                        validation.validationLength(obj, attr, true);
+                    	thisObj.validationLength(obj, attr, true);
                         
                         //대소문자 변경
-                        validation.toCase(obj, attr);
+                    	thisObj.toCase(obj, attr);
                     });
                 }
                 
@@ -164,7 +241,9 @@
             
             , checkTitle : function (obj, attr) {
                 if (!attr['title'] && !obj.getAttribute("title")) {
-                    alert('title 속성이 지정되어 있지 않습니다.');
+                	resultArr.push(obj);
+                	resultArr.push('title 속성이 지정되어 있지 않습니다.');
+//                    alert('title 속성이 지정되어 있지 않습니다.');
                     return false;
                 }
                 
@@ -182,8 +261,10 @@
                         return true;
                         break;
                     default :
-                        this.console(obj.nodeName+'은 사용 할 수 없습니다.');
-                        alert(obj.nodeName+'은 사용 할 수 없습니다.');
+//                        this.console(obj.nodeName+'은 사용 할 수 없습니다.');
+//                        alert(obj.nodeName+'은 사용 할 수 없습니다.');
+                    	resultArr.push(obj);
+                		resultArr.push(obj.nodeName+'은 사용 할 수 없습니다.');
                         return false;
                         break;
                 }
@@ -191,9 +272,12 @@
             
             , validationRequired : function (obj, attr) {
                 if (attr['required'] || attr['required'] === 'true') {
-                    if (this.isEmpty(obj.value)){
-                        alert(this.messageConverter(this.message['required'],this.thisTitle));
-                        this.errFocus(obj);
+                    if (this.isEmpty(obj.value)) {
+//                        alert(this.messageConverter(this.message['required'],this.thisTitle));
+//                        this.errFocus(obj);
+                    	
+                    	resultArr.push(obj);
+                		resultArr.push(this.messageConverter(this.message['required'],this.thisTitle));
                         return false;
                     } 
                 }
@@ -207,8 +291,10 @@
                     minLength = Number(minLength);
                     
                     if (!flag && obj.value.length < minLength) {
-                        alert(this.messageConverter(this.message['minLength'],this.thisTitle,[minLength]));
-                        this.errFocus(obj);
+//                        alert(this.messageConverter(this.message['minLength'],this.thisTitle,[minLength]));
+//                        this.errFocus(obj);
+                        resultArr.push(obj);
+                		resultArr.push(this.messageConverter(this.message['minLength'],this.thisTitle,[minLength]));
                         return false;
                     }
                 }
@@ -217,8 +303,10 @@
                     maxLength = Number(maxLength);
                     
                     if (!flag && obj.value.length > maxLength) {
-                        alert(this.messageConverter(this.message['maxLength'],this.thisTitle,[maxLength]));
-                        this.errFocus(obj);
+//                        alert(this.messageConverter(this.message['maxLength'],this.thisTitle,[maxLength]));
+//                        this.errFocus(obj);
+                        resultArr.push(obj);
+                		resultArr.push(this.messageConverter(this.message['maxLength'],this.thisTitle,[maxLength]));
                         return false;
                     }
                     else if (flag && obj.value.length > minLength) {
@@ -230,12 +318,14 @@
             }
             
             , toCase : function (obj, attr) {
-                if (!attr) return;
+                if (!attr) {
+                	return;
+                }
                 
-                if (attr['toCase'].toUpperCase() == 'UPPER'){
+                if (attr['toCase'].toUpperCase() === 'UPPER'){
                     obj.value = obj.value.toUpperCase();
                 }
-                else if (attr['toCase'].toUpperCase() == 'LOWER'){
+                else if (attr['toCase'].toUpperCase() === 'LOWER'){
                     obj.value = obj.value.toLowerCase();
                 }
             }
@@ -262,6 +352,6 @@
         };
 
 
- window.validate = {}
- ,window.validate = validate; 
+    window[fn] = window[fn] || {}
+   ,window[fn] = validate;
 })(window);
